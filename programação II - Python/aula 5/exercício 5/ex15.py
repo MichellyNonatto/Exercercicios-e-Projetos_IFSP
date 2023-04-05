75f5656b764891bb2ce7f3d3fad5e2fb9a165215
Michelly Nonatto <smartnovo01@gmail.com>
2023-04-05 20:01:24 -0300

    atualização 05/04

--- programação II - Python/aula 5/exercício 5/ex15.py
+++ programação II - Python/aula 5/exercício 5/ex15.py
@@ -1,1 +1,14 @@
 #Faça um programa que leia três valores que representam os lados de um triângulo. Primeiramente, verifique se os lados podem formar um triângulo ( a soma de dois lados não pode ser menor que o terceiro lado ). Caso possa formar um triângulo, indique se este é equilátero, isósceles ou escaleno.
+
+lados, igual = [], 0
+while len(lados) < 3:
+     lado = int(input("Insira o lado do triângulo: "))
+     lados.append(lado)
+
+if lados[0] + lados[1] >= lados[2] and lados[1] + lados[2] >= lados[0] and lados[2] + lados[0] >= lados[1]: print("É um triângulo")
+else: print("Não é um triângulo.\n")
+
+for i in lados:
+     
+
+print("equilátero.") if igual == len(lados) else print("não sei")
