849f7ba9131a1ff599a0a2c98b1c8c157a943cda
Michelly <smartnovo01@gmail.com>
2023-09-11 15:38:57 -0300

    atualização 11/09

--- Trabalho_estrutura_de_dados/main.c
+++ Trabalho_estrutura_de_dados/main.c
@@ -6,22 +6,23 @@
 #include "manipular_vetor.h"
 #include "algoritmo_ordenacao.h"
 
-#define TAM 10000 // Tamanho global do vetor utilizado em TODAS as funções
+#define TAM 100000 // Tamanho global do vetor utilizado em TODAS as funções
 
 int main()
 {
-    system("cls");   // Limpar tela no Windows
-    system("clear"); // Limpar tela no Linux
+    system("cls");
+    system("clear");
 
     setlocale(LC_ALL, "portuguese");
 
     clock_t start_time, end_time;
     double cpu_time_used;
 
-   // int numeros[TAM];
-    // NAS FUNÇÕES A SEGUIR ESCOLHA COMO VOCÊ QUER ORDENAR OS VALORES DENTRO DO ARRAY NUMEROS. a FUNÇÃO DESORDENADO GERA UMA SEQUÊNCIA DE NÚMEROS ALEATÓRIOS COM O RAND, JÁ A FUNÇÃO INVERTIDO GERA UMA SEUQUÊNCIA DECRESCENTE A PARTIR DO VALOR DECLARADO NA VARIÁVEL TAM.
+    int numeros[TAM];
 
-    // desordenado(numeros, TAM); // numeros[87, 20, 0, 57, 41, ...]
+    // NAS FUNÇÕES A SEGUIR ESCOLHA COMO VOCÊ QUER ORDENAR OS VALORES DENTRO DO ARRAY NUMEROS. A FUNÇÃO DESORDENADO GERA UMA SEQUÊNCIA DE NÚMEROS ALEATÓRIOS COM O RAND, JÁ A FUNÇÃO INVERTIDO GERA UMA SEQUÊNCIA DECRESCENTE A PARTIR DO VALOR DECLARADO NA VARIÁVEL TAM TERMINANDO COM O NÚMERO.
+
+     desordenado(numeros, TAM); // numeros[87, 20, 0, 57, 41, ...]
     // invertido(numeros, TAM); //   numeros[100, 99, 98, 97, 96, ...]
 
     // mostrarVetor(numeros, TAM); //  Utilize apenas para verificação da sequência retornada pelas funções de ordenação.
@@ -32,7 +33,7 @@ int main()
     // insert(numeros, TAM);
     // selection(numeros, TAM);
     // mergeSort(numeros, TAM);
-   //  printf("Total de operações realizadas: %d", quick(numeros, 0, (TAM - 1)));
+    // printf("Total de operações realizadas: %d", quick(numeros, 0, (TAM - 1)));
     end_time = clock();
 
     cpu_time_used = ((double)(end_time - start_time)) / CLOCKS_PER_SEC;
